6b8ef709f21e61178ba0033009c64d2f740e1ab5
Louis Lambrecht <lambrecht.louis@yahoo.fr>
2018-05-04 13:22:43 +0200

    add saver npy for test data and test data loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,8 +5,11 @@ import numpy as np
 
 
 
-patch_nu_path = "../DRIVE_datasets_training_testing/patchs_original/"
-patch_gt_path = "../DRIVE_datasets_training_testing/patchs_gt/"
+patch_nu_path = "../DRIVE_datasets_training_testing/train/patchs_original/"
+patch_gt_path = "../DRIVE_datasets_training_testing/train/patchs_gt/"
+
+patch_nu_test = "../DRIVE_datasets_training_testing/test/patchs_original/"
+patch_gt_test = "../DRIVE_datasets_training_testing/test/patchs_gt/"
 
 
 def DataCreator():
@@ -36,15 +39,46 @@ def DataCreator():
         imgs_gt[i] = arrayImg
         i += 1
 
+    #test data
+    imagestest = os.listdir(patch_nu_test)
+    totaltest = len(imagestest)
+
+    imgstest = np.ndarray((totaltest, 48,48), dtype=np.uint8)
+    imgstest_gt = np.ndarray((totaltest, 48,48), dtype= np.uint8)
+
+    #original tests
+    i = 0
+    for image_name in imagestest:
+        imgtest = Image.open(os.path.join(patch_nu_test, image_name))
+        arrayImgTest = np.asarray(imgtest)
+        imgstest[i] = arrayImgTest
+        i += 1
+
+    #ground truth test
+    imagestest = os.listdir(patch_gt_test)
+    i=0
+    for image_name in imagestest:
+        imgtestGT = Image.open(os.path.join(patch_gt_test, image_name))
+        arrayImgTestGt = np.asarray(imgtestGT)
+        imgstest_gt[i] = arrayImgTestGt
+        i+=1
+
+    #save train data
     np.save('../imgs_train.npy', imgs)
     np.save('../imgs_gt_train.npy', imgs_gt)
-    print('Saved data to .npy files')
+    print('Saved train data to .npy files')
 
+    #save test data
+    np.save('../imgs_test.npy', imgstest)
+    np.save('../imgs_gt_test.npy', imgstest_gt)
+    print('Saved train data to .npy files')
 
 def DataLoader():
     imgs_train = np.load('../imgs_train.npy')
     imgs_train_gt = np.load('../imgs_gt_train.npy')
     return imgs_train, imgs_train_gt
 
-
-    #groundtruth data
+def load_test_data():
+    imgs_test=np.load('../imgs_test.npy')
+    imgs_test_gt = np.load('../imgs_gt_test.npy')
+    return imgs_test, imgs_test_gt
